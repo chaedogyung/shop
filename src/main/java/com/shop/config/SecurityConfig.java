17ec9a716bb9f18bf64455bc2db094ddcd11c5a5
livew <livew@DESKTOP-J2ABFSI>
2023-05-17 11:48:37 +0900

    page 188

--- src/main/java/com/shop/config/SecurityConfig.java
+++ src/main/java/com/shop/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -23,14 +24,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.formLogin()
-		.loginPage("/member.login")
+		.loginPage("/members/login")
 		.defaultSuccessUrl("/")
 		.usernameParameter("email")
 		.failureUrl("/members/login/error")
 		.and()
 		.logout()
-		.logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
+		.logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))
 		.logoutSuccessUrl("/");
+		
+		http.authorizeRequests()
+		.mvcMatchers("/", "/members/**",
+				"/item/**", "/images/**").permitAll()
+		.mvcMatchers("/admin/**").hasRole("ADMIN")
+		.anyRequest().authenticated();
+		
+		http.exceptionHandling()
+		.authenticationEntryPoint(new CustomAuthenticationEntryPoint());
+	}
+	@Override
+	public void configure(WebSecurity web) throws Exception{
+		web.ignoring().antMatchers("/css", "/js/**", "/img/**");
 	}
 
 	@Bean
--- src/main/java/com/shop/controller/MemberController.java
+++ src/main/java/com/shop/controller/MemberController.java
@@ -1,9 +1,7 @@
 package com.shop.controller;
 
-import com.shop.dto.MemberFormDto;
-import com.shop.entity.Member;
-import com.shop.service.MemberService;
-import lombok.RequiredArgsConstructor;
+import javax.validation.Valid;
+
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,8 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.validation.Valid;
-
+import com.shop.dto.MemberFormDto;
+import com.shop.entity.Member;
+import com.shop.service.MemberService;
+import lombok.RequiredArgsConstructor;
 @RequestMapping("/members")
 @Controller
 @RequiredArgsConstructor
@@ -47,7 +47,7 @@ public class MemberController {
         return "redirect:/";
     }
 
-    @PostMapping(value = "/login")
+    @GetMapping(value = "/login")
     public String loginMember(){
         return "/member/memberLoginForm";
     }
